da3b84e2c6dbf05afe0c82576ccd6e318c5ba481
jaycedowell <jayce.dowell@gmail.com>
2024-09-08 17:04:16 -0600

    f-strings and fixes.

--- downloadQSMag.py
+++ downloadQSMag.py
@@ -5,7 +5,7 @@ import sys
 import time
 import zipfile
 import argparse
-import urllib.request import urlretrieve
+from urllib.request import urlretrieve
 
 
 def main(args):
@@ -21,27 +21,26 @@ def main(args):
         urlretrieve("https://www.prismnet.com/~mmccants/programs/qsmag.zip", "qsmag.zip")
         t1 = time.time()
         sz = os.path.getsize("qsmag.zip")
-        print("-> downloaded %i bytes in %.3f s (%.1f kB/s)" % (sz, t1-t0, sz/1024./(t1-t0)))
+        print(f"-> downloaded {sz} bytes in {t1-t0:.3f} s ({sz/1024/(t1-t0):.1f} kB/s)")
         
         print("Extracting to 'qs.mag'")
         qsmag = zipfile.ZipFile('qsmag.zip')
         
-        fh = open('qs.mag', 'w')
-        fh.write( qsmag.read('qs.mag') )
-        fh.close()
+        with open('qs.mag', 'wb') as fh:
+            fh.write( qsmag.read('qs.mag') )
         sz = os.path.getsize('qs.mag')
-        print("-> extracted %s bytes to 'qs.mag'" % sz)
+        print(f"-> extracted {sz} bytes to 'qs.mag'")
         
         ## Cleanup
         os.unlink('qsmag.zip')
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(
-		description='download and extract the QuickSat intrinsic magntiude file',
-		formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(
+        description='download and extract the QuickSat intrinsic magntiude file',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-	parser.add_argument('-f', '--force', action='store_true',
-						help='force re-downloading and extracting the data')
-	args = parser.parse_args()
-	main(args)
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='force re-downloading and extracting the data')
+    args = parser.parse_args()
+    main(args)
